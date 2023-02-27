9e68912581b07260084b96ea4653104e310517fa
Neto student1 <teststudent1@neto.com>
2023-02-27 20:24:03 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500; // стоимость билета
+        int bonus = 20; // стоимость одной бонусной мили
+        System.out.println("Количество бонусных миль:" + (amount/bonus));
+
+
+    }
+}
